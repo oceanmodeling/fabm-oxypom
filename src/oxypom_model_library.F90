2433dc74e9fafdb7313ba9c49fdfc3951b5613c4
Carsten Lemmen <carsten.lemmen@hereon.de>
2025-03-20 17:52:01 +0100

    fprettified F90 files

--- src/oxypom_model_library.F90
+++ src/oxypom_model_library.F90
@@ -15,27 +15,27 @@ module oxypom_model_library
       procedure :: create
    end type
 
-   type (type_factory), save, target, public :: oxypom_model_factory
+   type(type_factory), save, target, public :: oxypom_model_factory
 
 contains
 
    subroutine create(self, name, model)
-   use oxypom_oxypom
-   use oxypom_diamo
-   use oxypom_light
-   ! Add use statements for new models here
+      use oxypom_oxypom
+      use oxypom_diamo
+      use oxypom_light
+      ! Add use statements for new models here
 
-      class (type_factory), intent(in) :: self
-      character(*),         intent(in) :: name
-      class (type_base_model), pointer :: model
+      class(type_factory), intent(in) :: self
+      character(*), intent(in) :: name
+      class(type_base_model), pointer :: model
 
       select case (name)
          ! Add case statements for new models here
-         case ('oxypom'); allocate(type_oxypom_oxypom::model)
-         case ('diamo'); allocate(type_oxypom_diamo::model)
-         case ('light'); allocate(type_oxypom_light::model)
-         case default
-            call self%type_base_model_factory%create(name, model)
+      case ('oxypom'); allocate (type_oxypom_oxypom::model)
+      case ('diamo'); allocate (type_oxypom_diamo::model)
+      case ('light'); allocate (type_oxypom_light::model)
+      case default
+         call self%type_base_model_factory%create(name, model)
       end select
 
    end subroutine create
