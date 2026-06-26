726693abfdb96d966245e815080f01b957a14978
Ovidio Fernando Garcia Oliva <ovidio.garcia@hereon.de>
2026-06-26 11:05:20 +0200

    Edit .lintr.R license fix

--- testcases/temperature-sensitivity/.lintr.R
+++ testcases/temperature-sensitivity/.lintr.R
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025-2026 Helmholtz-Zentrum hereon GmbH
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileContributor Ovidio Garcia-Oliva <ovidio.garcia@hereon.de>
+
 linters <- linters_with_defaults(
   line_length_linter = NULL,
   commented_code_linter = NULL,
